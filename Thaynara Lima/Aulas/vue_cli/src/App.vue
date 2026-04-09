87f74103dcf2324c48e87e997d111931eb6b46f4
Thaynara Lima Soares Sousa <thaynara.ls@dinfor.uniceub>
2026-04-08 21:10:24 -0300

    aula da 08 Vue, celso e as torres

--- Thaynara Lima/Aulas/vue_cli/src/App.vue
+++ Thaynara Lima/Aulas/vue_cli/src/App.vue
@@ -5,9 +5,15 @@
     <hr />
     <HomeComponent />
     <hr>
-    <UsuarioComponent/>
-    <hr>
+    <UsuarioComponent v-show="componentUsuarioVisivel"/>
+    <br>
     <EstiloComponents/>
+    <br/>
+    <ImagemComponent
+    @visibilidade-img="escutarVisibilidadeImg"
+    larguraImg="500"
+    urlImagem="https://causaoperaria.org.br/wp-content/uploads/2022/09/32AF2063-72AC-4706-9750-47356C3736C7-942x720.jpeg"/>
+    
   </div>
 </template>
 
@@ -15,6 +21,8 @@
 import HomeComponent from "./components/HomeComponent.vue";
 import UsuarioComponent from "./components/UsuarioComponent.vue";
 import EstiloComponents from "./components/EstiloComponents.vue";
+import ImagemComponent from "./components/ImagemComponent.vue";
+
 
 export default {
 name: "App",
@@ -23,6 +31,19 @@ name: "App",
     HomeComponent,
     UsuarioComponent,
     EstiloComponents,
+    ImagemComponent,
+  
+  },
+  data() {
+    return {
+      componentUsuarioVisivel: true,
+    }
+  },
+
+  methods: {
+    escutarVisibilidadeImg(isVisivel) {
+      this.componentUsuarioVisivel = !isVisivel
+    },
   },
 
   created() {
